e521495857dd52c4163da375d424566ae7cbcc0c
LyraMaker <gabriel.lyrat@gmail.com>
2022-01-14 01:19:03 -0300

    Registrando no banco

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
     <h1 class="is-uppercase title">Formulario para gerar o banner</h1>
     <h2 class="subtitle has-text-danger">Complete o formulário com o que pede!</h2>
 </div>
-<form action="" method="post" class="block">
+<form action=" misc/gerar.php" method="post" class="block">
     <div class="field">
         <div class="control has-icons-left">
             <input type="text" class="input" name="nome" id="nome" placeholder="Nome do produto" required>
@@ -32,7 +32,7 @@
     </div>
     <div class="field is-grouped is-grouped-centered">
         <p class="control">
-            <input type="submit" class="button is-primary" value="Gerar oferta">
+            <input type="submit" class="button is-primary" value="Gerar oferta" >
                     </p>
         <p class="control">
             <a class="button is-warning">
---
+++ misc/gerar.php
@@ -0,0 +1,16 @@
+<?php
+
+include('conec.php');
+$nome = $_POST['nome']; $precoAntigo = $_POST['preco-antigo']; $desconto = $_POST['desconto']; $obs = $_POST['obs'];
+
+$query = "INSERT INTO promocao(produto,precoAntigo,desconto,observacao) values ( :produto,:precoAntigo,:desconto,:observacao);";
+
+$stmt = $pdo->prepare($query);
+
+$stmt->bindValue(":produto",$nome);
+$stmt->bindValue(":precoAntigo",$precoAntigo);
+$stmt->bindValue(":desconto",$desconto);
+$stmt->bindValue(":observacao",$obs);
+
+$stmt->execute();
+?>
